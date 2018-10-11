966005c0561ae7862ecc635ff403fc442d97fe18
Sylwia Sadowska <sylwiaxsadowska@gmail.com>
2018-10-11 16:09:11 +0200

    sprawdzenie liczby

---
+++ main.js
@@ -0,0 +1,10 @@
+function number(n) {
+   
+    if (n>100 && n<200) {
+        console.log('Liczba znajduje się w przedziale');
+    }
+    else if (n<100 && n>200) {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+number(152);
